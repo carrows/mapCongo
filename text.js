86c3d99171daa1f4c8bad809335e4b912c92edb1
carrows <carrow.kalalamasela@gmail.com>
2015-03-31 15:42:38 +1400

    Create text.js

---
+++ text.js
@@ -0,0 +1,9 @@
+<!DOCTYPE Html>
+<html>
+   <head>
+      <title>c'</title>
+   </head>
+   <body>
+   <script src="http://maps.google.com/maps?file=api&v=2&key="type="text/javascript"></script>
+   </body>
+ </html>
